6014c05592890716b29f405313d7f81c28fcbfb7
Ihor M. <mukovoz.igor@gmail.com>
2026-08-26 15:40:11 +0300

    refactor: modularize command handling in separate module
    - Moved contact-related functions to `commands.py` for better modularity.
    - Updated `main.py` to import and use functions from `commands.py`.

---
+++ commands.py
@@ -0,0 +1,87 @@
+from decorators.Input import input_error
+from modules.AddressBook import AddressBook
+from modules.Record import Record
+
+
+@input_error
+def add_contact(args: list[str], book: AddressBook) -> str:
+    name, phone, *_ = args
+
+    record = book.find(name)
+    message = "Contact updated."
+
+    if record is None:
+        record = Record(name)
+        record.add_phone(phone)
+        book.add_record(record)
+        return "Contact added."
+
+    record.add_phone(phone)
+    return message
+
+
+@input_error
+def change_contact(args: list[str], book: AddressBook) -> str:
+    name, phone, new_phone = args
+
+    contact = book.find(name)
+
+    if not contact:
+        return "Contact not found."
+
+    res = contact.edit_phone(phone, new_phone)
+
+    return "Contact updated." if res else "Nothin' changed. Phone not found."
+
+
+@input_error
+def show_phone(args: list[str], book: AddressBook) -> Record | str:
+    name = args[0]
+    record = book.find(name)
+
+    return record if record else "Contact not found."
+
+
+@input_error
+def show_all(book: AddressBook) -> str:
+    if not book:
+        return "No contacts saved."
+
+    return "\n".join(f"{name}: {record}" for name, record in book.items())
+
+
+@input_error
+def add_birthday(args: list[str], book: AddressBook) -> str:
+    name, birthday = args
+
+    record = book.find(name)
+    if not record:
+        return "Contact not found."
+
+    record.add_birthday(birthday)
+
+    return "Birthday added."
+
+
+@input_error
+def show_birthday(args: list[str], book: AddressBook) -> str:
+    name = args[0]
+
+    record = book.find(name)
+    if not record:
+        return "Contact not found."
+
+    if not record.birthday:
+        return "Birthday not set."
+
+    return str(record.birthday)
+
+
+@input_error
+def birthdays(book: AddressBook) -> str:
+    upcoming = book.get_upcoming_birthdays()
+
+    if not upcoming:
+        return "No birthdays in the next week."
+
+    return "\n".join(f"{u['name']}: {u['congratulation_date']}" for u in upcoming)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from decorators.Input import input_error
+import commands
+
 from modules.AddressBook import AddressBook
 from modules.PersistStorage import PersistStorage
-from modules.Record import Record
 
 
 def parse_input(user_input: str) -> tuple[str, list[str]]:
@@ -11,89 +11,6 @@ def parse_input(user_input: str) -> tuple[str, list[str]]:
 
     cmd, *args = parts
     return cmd.lower(), args
-
-@input_error
-def add_contact(args: list[str], book: AddressBook) -> str:
-    name, phone, *_ = args
-
-    record = book.find(name)
-    message = "Contact updated."
-
-    if record is None:
-        record = Record(name)
-        record.add_phone(phone)
-        book.add_record(record)
-        return "Contact added."
-
-    record.add_phone(phone)
-    return message
-
-
-@input_error
-def change_contact(args: list[str], book: AddressBook) -> str:
-    name, phone, new_phone = args
-
-    contact = book.find(name)
-
-    if not contact:
-        return "Contact not found."
-
-    res = contact.edit_phone(phone, new_phone)
-
-    return "Contact updated." if res else "Nothin' changed. Phone not found."
-
-
-@input_error
-def show_phone(args: list[str], book: AddressBook) -> Record | str:
-    name = args[0]
-    record = book.find(name)
-
-    return record if record else "Contact not found."
-
-
-@input_error
-def show_all(book: AddressBook) -> str:
-    if not book:
-        return "No contacts saved."
-
-    return "\n".join(f"{name}: {record}" for name, record in book.items())
-
-
-@input_error
-def add_birthday(args: list[str], book: AddressBook) -> str:
-    name, birthday = args
-
-    record = book.find(name)
-    if not record:
-        return "Contact not found."
-
-    record.add_birthday(birthday)
-
-    return "Birthday added."
-
-
-@input_error
-def show_birthday(args: list[str], book: AddressBook) -> str:
-    name = args[0]
-
-    record = book.find(name)
-    if not record:
-        return "Contact not found."
-
-    if not record.birthday:
-        return "Birthday not set."
-
-    return str(record.birthday)
-
-
-@input_error
-def birthdays(book: AddressBook) -> str:
-    upcoming = book.get_upcoming_birthdays()
-
-    if not upcoming:
-        return "No birthdays in the next week."
-
-    return "\n".join(f"{u['name']}: {u['congratulation_date']}" for u in upcoming)
 
 
 def main():
@@ -116,19 +33,19 @@ def main():
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(add_contact(args, book))
+            print(commands.add_contact(args, book))
         elif command == "change":
-            print(change_contact(args, book))
+            print(commands.change_contact(args, book))
         elif command == "phone":
-            print(show_phone(args, book))
+            print(commands.show_phone(args, book))
         elif command == "all":
-            print(show_all(book))
+            print(commands.show_all(book))
         elif command == "add-birthday":
-            print(add_birthday(args, book))
+            print(commands.add_birthday(args, book))
         elif command == "show-birthday":
-            print(show_birthday(args, book))
+            print(commands.show_birthday(args, book))
         elif command == "birthdays":
-            print(birthdays(book))
+            print(commands.birthdays(book))
         else:
             print("Invalid command. Available commands: hello, add, change, phone, all, add-birthday, show-birthday, birthdays, close, exit")
 
